938264dbd6434f3140ff91fa8146ffb12e4cc191
Akila Mendis <godomainz@gmail.com>
2024-08-11 17:45:11 +0530

    68. Configuring a Column

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question ...'),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer1')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer2')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer3')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer4')),
+        ],
+      ),
+    );
   }
 }
